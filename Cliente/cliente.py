8bb7094a09ef4e89dafe4424a53d080205a93184
lucaasleal <lucasfaleal@gmail.com>
2026-04-24 14:03:04 -0300

    feat: Envio de arquivos improvisado

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -4,10 +4,15 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = input('digita ai lucas:')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+bufferSize = 1024
 
-modifiedMessage, serverAddres = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+fileName = "pasta/arquivo.txt"
+with open(fileName, 'rb') as f:
+    pct = f.read(bufferSize)
+    while pct:
+        print("Palavra: ", pct)
+        clientSocket.sendto(pct, (serverName, serverPort))
+        pct = f.read(bufferSize)
 
+print("Arquivo retornado com sucesso!")
 clientSocket.close()
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,12 +1,19 @@
 from socket import *
+import os
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
+fileName = "arquivo_serv.txt"
+bufferSize = 1024
 
 print('The server is ready to receive')
 
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    with open(fileName, 'wb') as f:
+        while True:
+            msg, servidor = serverSocket.recvfrom(bufferSize)
+            print("msg: ", msg)
+            if msg == b'EOF':
+                break
+            f.write(msg)

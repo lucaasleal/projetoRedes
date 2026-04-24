55b9dba8577181e2caf881ccb06fd06ffa3b4eea
JotagaXD <joaohenriquemg321@gmail.com>
2026-04-24 15:45:31 -0300

    servidorsalvando

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -6,13 +6,21 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 bufferSize = 1024
 
-fileName = "pasta/arquivo.txt"
-with open(fileName, 'rb') as f:
+fileName = "arquivo.txt"
+
+with open('pasta/' + fileName, 'rb') as f:
+
+    # envia o nome do arquivo para ser renomeado no servidor
+    clientSocket.sendto(fileName.encode(), (serverName, serverPort))
+
     pct = f.read(bufferSize)
     while pct:
         print("Palavra: ", pct)
         clientSocket.sendto(pct, (serverName, serverPort))
         pct = f.read(bufferSize)
+    
+    # envia o caracter null para sinalizar o servidor o fim do arquivo
+    clientSocket.sendto(b'', (serverName, serverPort))
 
 print("Arquivo retornado com sucesso!")
 clientSocket.close()
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,19 +1,30 @@
 from socket import *
-import os
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
-fileName = "arquivo_serv.txt"
 bufferSize = 1024
 
 print('The server is ready to receive')
 
 while True:
+
+    # recebe o nome do arquivo
+    msg, servidor = serverSocket.recvfrom(bufferSize)
+    
+    # cria o novo nome do arquivo (ex: arquivo_leilao.txt)
+    fileName = msg.decode().split('.')[0] + '_leilao.txt'
+    
+    # recebe o conteúdo do arquivo e cria um novo arquivo com o mesmo nome
+    # escreve o conteúdo dos pacotes recebidos no novo arquivo
     with open(fileName, 'wb') as f:
+        msg, servidor = serverSocket.recvfrom(bufferSize)
         while True:
-            msg, servidor = serverSocket.recvfrom(bufferSize)
             print("msg: ", msg)
-            if msg == b'EOF':
-                break
             f.write(msg)
+            msg, servidor = serverSocket.recvfrom(bufferSize)
+            if msg == b'':
+                f.write(b'')
+                break
+    
+    

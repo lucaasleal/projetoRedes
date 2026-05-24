a404e808cab7a8a5d9da65769a5532ec3941ed80
PeterIgnatious <pias@cin.ufpe.br>
2026-05-24 12:33:41 -0300

    Implementação do Gerador de Perdas
    Tome

---
+++ cliente.py
@@ -0,0 +1,155 @@
+# Módulo cliente.py: Responsável por enviar os arquivos para o servidor e
+#                    receber os arquivos renomeados do servidor, salvando-os na pasta
+
+import socket # importa a biblioteca socket para criar o socket UDP e realizar a comunicação com o servidor
+import time
+from random import random
+
+SERVER_NAME = 'localhost' # nome do servidor
+SERVER_PORT = 12000 # porta do servidor
+BUFFER_SIZE = 1024 # tamanho do buffer para leitura dos arquivos (1KB)
+HEADER_SIZE = 1
+LIST_FILES = [
+    'atumalaca.jpg',
+    'poema.txt',
+    'hold_the_line.mp3',
+    'boa_tarde_neymar.mp4'
+] # lista de arquivos a serem enviados e tratados pelo servidor
+PACKET_LOSS = 0.005
+
+class Client:
+    def __init__(self, server_name, server_port, buffer_size, header_size, list_files):
+        self.server_name = server_name
+        self.server_port = server_port
+        self.buffer_size = buffer_size
+        self.header_size = header_size
+        self.list_files = list_files
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # cria o socket UDP do cliente
+        self.sequence_number = 0
+        self.ack_number = 1
+        self.data_size = buffer_size - header_size
+        self.package_number = 0
+            
+
+    def create_segment(self, data):
+        sequence_number_b = self.sequence_number.to_bytes(1)
+        
+        return sequence_number_b + data
+    
+
+    def send_segment(self, data):
+        if (random() >= PACKET_LOSS):
+            segment = self.create_segment(data)
+            self.socket.sendto(segment, (self.server_name, self.server_port))
+    
+    
+    def send_rec_segment(self, data):
+        self.send_segment(data)
+        
+        while True:
+            try:
+                ack, _ = self.socket.recvfrom(self.buffer_size)
+                ack_number = ack[0]
+                
+                if ack_number == self.sequence_number:
+                    self.sequence_number = (self.sequence_number + 1) % 2
+                    self.package_number += 1
+                    
+                    break
+                else:
+                    self.send_segment(data)
+            except:
+                print(f"Pacote {self.package_number} Perdido, enviando novamente...")
+                self.send_segment(data)
+
+
+    def send_file(self, file_name: str):
+        self.package_number = 0
+        self.socket.settimeout(.1);
+        self.send_rec_segment(file_name.encode())
+        
+        ## Rotina que abre o arquivo para leitura em modo binário e envia-o em pacotes para o servidor
+        with open('pasta/' + file_name, 'rb') as file:
+            ## Laço que envia os pacotes do arquivo para o servidor enquanto houver conteúdo para ler
+            while True:
+                data = file.read(self.data_size) # lê o conteúdo do arquivo em pacotes do tamanho do buffer
+                
+                if data:
+                    self.send_rec_segment(data) # envia o pacote para o servidor
+                else:
+                    break
+            
+        self.send_rec_segment(b'') # envia o caractere null para sinalizar o servidor do fim do arquivo
+
+        print(f"Número de pacotes enviados e reconhecidos: {self.package_number}")
+    
+
+
+    def send_ack(self):
+        self.socket.sendto(self.ack_number.to_bytes(1), (self.server_name, self.server_port))
+    
+    def extract_segment(self):
+        msg, _ = self.socket.recvfrom(self.buffer_size) # recebe o nome do arquivo renomeado pelo servidor
+        
+        return msg[0], msg[1:]
+
+
+    def extract_rec_segment(self):
+        while True:
+            seq_server_number, data = self.extract_segment()
+            
+            if seq_server_number != self.ack_number:
+
+                self.ack_number = seq_server_number
+                self.package_number += 1
+
+                self.send_ack()
+                                
+                return data
+            else:
+                self.send_ack()
+    
+       
+    def receive_file(self):
+        self.ack_number = 1
+        self.package_number = 0 # reseta o contador de pacotes recebidos
+        self.socket.settimeout(100);
+
+        file_renamed = self.extract_rec_segment()
+        
+        ## Rotina que recebe os pacotes do arquivo renomeado enviado pelo servidor e escreve o conteúdo em um novo arquivo (com nome novo)
+        with open('pasta/' + file_renamed.decode(), 'wb') as file:
+            ## Laço que recebe os pacotes do arquivo renomeado enviado pelo servidor enquanto houver conteúdo para ler, escrevendo o conteúdo dos pacotes recebidos no novo arquivo criado
+            while True:
+                data = self.extract_rec_segment()
+
+                if data == b'': # condição que sinaliza o fim do arquivo renomeado enviado pelo servidor
+                    break
+                else:
+                    file.write(data) # escreve o conteúdo do pacote recebido no novo arquivo criado
+
+            print(f"Arquivo {file_renamed.decode()} retornado com sucesso!")
+            print(f"Número de pacotes recebidos e reconhecidos: {self.package_number}")
+            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+        
+        
+    def run(self):
+        ## Laço para enviar e receber os arquivos
+        for fileName in self.list_files:
+
+            ## ENVIO DOS ARQUIVOS ##
+            self.send_file(fileName)
+
+            ## RECEBIMENTO DOS ARQUIVOS ##
+            self.receive_file()
+    
+    
+    def close(self):
+        self.socket.close() # fecha o socket após o envio e recebimento de todos os arquivos
+    
+            
+client1 = Client(SERVER_NAME, SERVER_PORT, BUFFER_SIZE, HEADER_SIZE, LIST_FILES)
+
+client1.run() 
+client1.close()
---
+++ servidor.py
@@ -0,0 +1,156 @@
+# Módulo servidor.py: Responsável por receber os arquivos enviados pelo cliente, salvando-os na pasta,
+#                     renomear esses arquivos e retorná-los para o cliente
+
+
+import socket # importa a biblioteca socket para criar o socket UDP e realizar a comunicação com o cliente
+import os #importa a biblioteca do sistema para criação do diretório para salvamento de arquivos no servidor
+from random import random
+
+SERVER_NAME = 'localhost'
+SERVER_PORT = 12000 # porta do servidor
+BUFFER_SIZE = 1024 # tamanho do buffer para leitura dos arquivos (1KB)
+HEADER_SIZE = 1
+PACKET_LOSS = 0.005
+
+class Server:
+    def __init__(self, server_name, server_port, buffer_size, header_size):
+        self.server_name = server_name
+        self.server_port = server_port
+        self.buffer_size = buffer_size
+        self.header_size = header_size
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # cria o socket UDP do cliente
+        self.sequence_number = 0
+        self.ack_number = 1
+        self.data_size = buffer_size - header_size
+        self.package_number = 0
+
+        self.socket.bind(('', SERVER_PORT)) # vincula o socket à porta definida
+        self.create_dir()
+    
+    def create_dir(self):
+        dir_name = "pasta"
+
+        # Verifica se não existe antes de criar
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+            print(f"'{dir_name}' criada.")
+        else:
+            print(f"'{dir_name}' já existe.")
+
+        print('O servidor está pronto para receber conexões!')
+
+    def send_ack(self, client):        
+        self.socket.sendto(self.ack_number.to_bytes(1), client)
+    
+    def extract_segment(self):
+        msg, client = self.socket.recvfrom(self.buffer_size) # recebe o nome do arquivo renomeado pelo servidor
+
+        return msg[0], msg[1:], client
+
+    def extract_rec_segment(self):
+        while True:
+            sequence_number, data, client = self.extract_segment()
+
+            if sequence_number != self.ack_number:
+                self.ack_number = sequence_number
+                self.package_number += 1
+
+                self.send_ack(client)
+                
+                return data, client
+            else:
+                self.send_ack(client)
+                
+
+    def receive_file(self):
+        self.sequence_number = 0
+        self.package_number = 0 # reseta o contador de pacotes recebidos
+        self.socket.settimeout(10);
+
+        file_name, client = self.extract_rec_segment()
+        print(file_name)
+        file_name = file_name.decode()
+        
+        ## Rotina que recebe os pacotes do arquivo enviado pelo cliente e escreve o conteúdo em um novo arquivo
+        with open('pasta/' + file_name, 'wb') as file:
+            ## Laço que recebe os pacotes do arquivo enviado pelo cliente enquanto houver conteúdo para ler, escrevendo o conteúdo dos pacotes no novo arquivo criado
+            while True:
+                data, client = self.extract_rec_segment()
+                
+                if data == b'': # condição que sinaliza o fim do arquivo enviado pelo cliente                    
+                    break
+                else:
+                    file.write(data) # escreve o conteúdo do pacote recebido no novo arquivo criado
+
+            print(f"Número de pacotes recebidos: {self.package_number}")
+            print(f"Arquivo {file_name} recebido com sucesso!")
+        
+        return client, file_name
+
+
+    def create_segment(self, data: str):
+        sequence_number_b = self.sequence_number.to_bytes(1)
+        return sequence_number_b + data
+    
+
+    def send_segment(self, data: str, client):
+        if (random() >= PACKET_LOSS):
+            segment = self.create_segment(data)
+            self.socket.sendto(segment, client)
+    
+    
+    def send_rec_segment(self, data: str, client):
+        self.send_segment(data, client)
+        
+        while True:
+            try:
+                ack, _ = self.socket.recvfrom(self.buffer_size)
+                ack_number = ack[0]
+                
+                if ack_number == self.sequence_number:
+                    self.sequence_number = (self.sequence_number + 1) % 2
+                    self.package_number += 1
+                    
+                    break
+                else:
+                    self.send_segment(data, client)
+            except:
+                print(f"Pacote {self.package_number} Perdido, enviando novamente...")
+                self.send_segment(data, client)
+
+    def send_file(self, client, fileName):
+        ## RETORNO DOS ARQUIVOS
+        self.package_number = 0 # reseta o contador de pacotes enviados
+        self.socket.settimeout(.1);
+
+        ## Rotina que abre o arquivo para leitura em modo binário, renomea-o e envia-o em pacotes para o cliente
+        with open('pasta/' + fileName, 'rb') as file:
+            file_renamed = 'leilao_' + fileName ## tratamento para enviar o arquivo renomeado para o cliente
+
+            self.send_rec_segment(file_renamed.encode(), client)
+
+            package = file.read(self.data_size) # lê o conteúdo do arquivo em pacotes do tamanho do buffer
+
+            ## Laço que envia os pacotes do arquivo para o cliente enquanto houver conteúdo para ler
+            while package:
+                self.send_rec_segment(package, client) # envia o pacote para o cliente
+                package = file.read(self.data_size) # lê o próximo pacote do arquivo até o final do arquivo
+
+            self.send_rec_segment(b'', client) # envia o caractere null para sinalizar o cliente do fim do arquivo
+
+            print(f"Arquivo {file_renamed} retornado com sucesso!")
+            print(f"Número de pacotes enviados: {self.package_number}")
+            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    
+
+    def run(self):
+        while True:
+            client, file_name = self.receive_file()
+            self.send_file(client, file_name)
+        
+        
+
+server = Server(SERVER_NAME, SERVER_PORT, BUFFER_SIZE, HEADER_SIZE)
+
+server.run()
